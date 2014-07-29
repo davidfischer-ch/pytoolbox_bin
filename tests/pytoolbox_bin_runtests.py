78ab71ff797a612c23121f9a7b16d203c88b9604
David Fischer <david.fischer.ch@gmail.com>
2014-07-29 11:55:00 +0200

    Use encoding utf-8 and not coding utf-8

--- tests/pytoolbox_bin_runtests.py
+++ tests/pytoolbox_bin_runtests.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 
 #**********************************************************************************************************************#
 #                    PYTOOLBOX BIN - PERSONAL UTILITY SCRIPTS BASED ON PYTOOLBOX AND OTHER GOODIES
