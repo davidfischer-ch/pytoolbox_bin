b1536094e183015867489264503a92346617cd05
David Fischer <david.fischer.ch@gmail.com>
2015-05-16 06:59:29 +0000

    Update version

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ Operating System :: Unix
 """
 
 setup(name='pytoolbox_bin',
-      version='0.3.2-beta',
+      version='0.3.3',
       packages=find_packages(exclude=['tests']),
       description='Personal utility scripts based on pytoolbox and other goodies.',
       long_description=open('README.rst', 'r', encoding='utf-8').read(),
